4a5168f3b258f32791fd2d651fb5ed27b6cf8689
Eric Akakpo <eric.akakpo.africatracing@outlook.com>
2022-06-17 15:07:44 +0000

    6-Programmatic_Navigation-end

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,21 @@
 <template>
-    <p>Register to the event</p>
+  <p>Register to the event</p>
+
+  <button v-on:click="register">Register</button>
+  
 </template>
 
 <script>
 export default {
-  props: ["event"]
+  props: ["event"],
+  methods: {
+    register() {
+      console.log(this.$router.params);
+      this.$router.push({
+        name: 'EventDetails',
+        //params: { id: this.event.id } : not needed
+      })
+    }
+  }
 };
 </script>
