59b60ce206ec58e840d1dd242de2b3d9f9b2bb9d
Eric Akakpo <eric.akakpo.africatracing@outlook.com>
2022-06-28 17:27:55 +0000

    Vuex Fundamentals - 3- Updating State

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,70 +1,48 @@
 <template>
-<h1>Create an event</h1>
+  <h1>Create an event</h1>
 
-<div class="form-container">
+  <div class="form-container">
 
-  <form @submit.prevent="onSubmit">
-    <label>Select a category: </label>
-    <select v-model="event.category">
-      <option
-        v-for="option in categories"
-        :value="option"
-        :key="option"
-        :selected="option === event.category"
-      >{{ option }}</option>
-    </select>
+    <form @submit.prevent="onSubmit">
+      <label>Select a category: </label>
+      <select v-model="event.category">
+        <option v-for="option in categories" :value="option" :key="option" :selected="option === event.category">{{
+            option
+        }}</option>
+      </select>
 
-    <h3>Name & describe your event</h3>
+      <h3>Name & describe your event</h3>
 
-    <label>Title</label>
-    <input
-      v-model="event.title"
-      type="text"
-      placeholder="Title"
-    >
+      <label>Title</label>
+      <input v-model="event.title" type="text" placeholder="Title">
 
-    <label>Description</label>
-    <input
-      v-model="event.description"
-      type="text"
-      placeholder="Description"
-    />
+      <label>Description</label>
+      <input v-model="event.description" type="text" placeholder="Description" />
 
-    <h3>Where is your event?</h3>
+      <h3>Where is your event?</h3>
 
-    <label>Location</label>
-    <input
-      v-model="event.location"
-      type="text"
-      placeholder="Location"
-    />
+      <label>Location</label>
+      <input v-model="event.location" type="text" placeholder="Location" />
 
-    <h3>When is your event?</h3>
-    <label>Date</label>
-    <input
-      v-model="event.date"
-      type="text"
-      placeholder="Date"
-    />
+      <h3>When is your event?</h3>
+      <label>Date</label>
+      <input v-model="event.date" type="text" placeholder="Date" />
 
-    <label>Time</label>
-    <input
-      v-model="event.time"
-      type="text"
-      placeholder="Time"
-    />
+      <label>Time</label>
+      <input v-model="event.time" type="text" placeholder="Time" />
 
-    <button type="submit">Submit</button>
-  </form>
+      <button type="submit">Submit</button>
+    </form>
 
-</div>
+  </div>
 </template>
 
 <script>
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid"
+import EventService from '@/services/EventService.js'
 
 export default {
-  data () {
+  data() {
     return {
       categories: [
         'sustainability',
@@ -89,9 +67,21 @@ export default {
   },
   methods: {
     onSubmit() {
-      this.event.id = uuidv4()
-      this.event.organizer = this.$store.state.user
-      console.log("Event:", this.event)
+      const event = {
+        ...this.event,
+        id : uuidv4(),
+        organizer : this.$store.state.user
+      }
+      //this.event.id = uuidv4()
+      //this.event.organizer = this.$store.state.user
+      EventService
+        .postEvent(event)
+        .then(() => {
+          this.$store.commit('ADD_EVENT', event)
+        })
+        .catch(error => {
+
+        })
     }
   }
 }
